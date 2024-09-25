266f828c798af1c40853d6ecb612e1bdfe28d43b
IvanSharai <ivansharai99@gmail.com>
2024-09-25 11:05:29 +0200

    added simple filter chain

--- src/main/java/org/example/foodappapi/config/SecurityConfig.java
+++ src/main/java/org/example/foodappapi/config/SecurityConfig.java
@@ -1,5 +1,22 @@
 package org.example.foodappapi.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
 public class SecurityConfig {
 
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount", "/myCart").authenticated()
+                .requestMatchers("/home", "/store", "/contact", "/error").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
 }
